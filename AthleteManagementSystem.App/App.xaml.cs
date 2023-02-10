fde4e5dc09f05ab1f3dca59963d69a142977c334
Hasan3790 <hasanrezaei3790@gmail.com>
2023-02-10 09:19:36 +0330

    first commit

---
+++ AthleteManagementSystem.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AthleteManagementSystem.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
